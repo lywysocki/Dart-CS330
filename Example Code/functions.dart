370622353bf04434848dd6fc9f898649cc0cd4dd
lywysocki <lywysocki@gmail.com>
2023-12-05 19:04:32 -0500

    Finished PLP3 - Code Examples

--- Example Code/functions.dart
+++ Example Code/functions.dart
@@ -6,10 +6,22 @@
  * - Write a function that tests whether your language is pass-by-reference or pass-by-value.
  */
 
-// Write a function that takes in two numbers, multiplies them, and returns the output
+// A function that takes in two numbers, multiplies them, and returns the output
+int multiply(int num1, int num2) {
+  return num1 * num2;
+}
+
+void main() {
+  int n1 = 6;
+  int n2 = 7;
+
+  int prod = multiply(n1, n2);
+
+  print('The product of $n1 * $n2 is $prod');
+}
 
 
-// Write a recursive function (one that calculates a factorial is fine)
+// A recursive function which calculates a factorial
 int factorial(int n) {
   if (n <= 1) {
     return 1;
@@ -18,14 +30,52 @@ int factorial(int n) {
   }
 }
 
-void main() {
+void factorialMain() {
   int number = 5;
   int result = factorial(number);
+
   print('Factorial of $number is $result');
 }
 
-// Write a function that takes in a string (or your language's equivalent) and splits it into two strings, then returns both strings
 
-// Call your functions and save the results of the function calls in variables.
+// A function that takes in a string and splits it into two strings, then returns both strings
+List<String> splitString(String input, int splitIndex) {
 
-// Write a function that tests whether your language is pass-by-reference or pass-by-value.
+  if (splitIndex >= 0 && splitIndex < input.length) {
+    String firstPart = input.substring(0, splitIndex);
+    String secondPart = input.substring(splitIndex);
+
+    return [firstPart, secondPart];
+
+  } else {
+    return [input, ''];
+  }
+}
+
+void splitMain() {
+  String originalString = 'HelloWorld!';
+  int splitIndex = 5;
+
+  List<String> result = splitString(originalString, splitIndex);
+
+  print('First part: ${result[0]}');
+  print('Second part: ${result[1]}');
+}
+
+
+// A function that tests whether Dart is pass-by-reference or pass-by-value
+void testFunction(int number, List<int> list) {
+  number = 20; // Modifying the local 'number' (doesn't affect the original value)
+  list[0] = 100; // Modifying the list (affects the original list)
+}
+
+void testMain() {
+  int num = 10;
+  List<int> myList = [1, 2, 3];
+
+  print('Before function call: a = $num, myList = $myList'); // Prints initial values
+
+  testFunction(num, myList);
+
+  print('After function call: a = $num, myList = $myList'); // Prints values after function call
+}
--- Example Code/loops.dart
+++ Example Code/loops.dart
@@ -1,22 +1,66 @@
 /**
- *
- * - Write (or try to emulate, if your language doesn’t have them) different kinds of loops: while, do/while, for, foreach
- *
+ * A few more examples of each of the five types of loops in Dart
  */
-
 class Loops {
-  bool i_am_true = true;
 
-  int whileExample(bool b) {
+  void whileExample() {
     int counter = 0;
 
-    while (b) {
+    // while loop
+    while (counter < 5) {
+      print('I am loop #${counter + 1} of the while loop');
       counter++;
-      if (counter > 10) {
-        break;
-      }
     }
-    return counter;
   }
+
+  void doWhileExample() {
+    int count = 0;
+
+    // do-while loop
+    do {
+      print('I am loop #${count + 1} of the do-while loop');
+      count++;
+    } while (count < 5);
+  }
+
+  void forExample() {
+    // for loop
+    for (int i = 0; i < 5; i++) {
+      print('I am loop #$i of the for loop');
+    }
+  }
+
+  void forInExample() {
+    List<int> myList = [1, 2, 3, 4, 5];
+
+    // for-in loop
+    for (var element in myList) {
+      print('I am element #${myList.indexOf(element)} with value of $element in the for-in loop');
+    }
+  }
+
+  void forEachExample() {
+    Map<int, String> myMap = {
+      1 : 'apple',
+      2 : 'watermelon',
+      3 : 'mango',
+    };
+
+    // forEach() loop
+    myMap.forEach((key, value) {
+      print('This is a key: $key and  it\'s value: $value within the map');
+    });
+  }
+}
+
+// main function for running functions
+void main() {
+  // To run each example:
+  // Class().function();
+  Loops().whileExample();
+  Loops().doWhileExample();
+  Loops().forExample();
+  Loops().forInExample();
+  Loops().forEachExample();
 
 }
