948685dad7afc830d211c28a288a623f81c29db8
lywysocki <lywysocki@gmail.com>
2023-12-16 15:39:45 -0500

    updates

--- Example Code/helloworld.dart
+++ Example Code/helloworld.dart
@@ -5,3 +5,7 @@ void main() {
   // Prints "Hello World!" to the console
   print('Hello World!');
 }
+
+// OR you can use the shorthand syntax
+// just uncomment to see how it works
+// void main() => print('Hello, World!');
